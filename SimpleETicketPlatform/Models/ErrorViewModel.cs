a6c341594e6c9ae90acfd188f549a96fc45c90cb
yassyolo <yoana.yotova03@gmail.com>
2024-07-03 14:32:27 +0300

    Add project files.

---
+++ SimpleETicketPlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SimpleETicketPlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
